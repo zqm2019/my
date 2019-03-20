eda2a5f3aafe9553353e3a4b63250a4f453478b1
zhaqianming <zhaqianming@tuniu.com>
2019-03-20 22:24:23 +0800

    build project

---
+++ src/main.java
@@ -0,0 +1,15 @@
+/**
+ * Copyright (C) 2006-2019 Tuniu All rights reserved
+ */
+
+/**
+ * TODO: description
+ * Date: 2019-03-20
+ *
+ * @author zhaqianming
+ */
+public class main{
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
